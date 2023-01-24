f62d864844ce550d1b837427000cabfa6354af39
kwamea236 <kwamea236@protonmail.com>
2023-01-24 00:23:48 +0000

    initial commit

---
+++ pages/src/Homepage.jsx
@@ -0,0 +1,9 @@
+function Homepage(){
+    return(
+        <div className="mx-4 my-4">
+            <button className="px-6 py-6 bg-red-500 rounded shadow-2xl">Welcome</button>
+        </div>
+    )
+}
+
+export default Homepage;
