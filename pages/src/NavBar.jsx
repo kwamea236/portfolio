c7411290276376b7e0112eaf84f1f27576ecd375
kwamea236 <kwamea236@protonmail.com>
2023-01-25 01:30:13 +0000

    moonfill button fixed

--- pages/src/NavBar.jsx
+++ pages/src/NavBar.jsx
@@ -7,7 +7,7 @@ function NavBar({darkMode, setDarkMode}){
              <nav className="py-6 mb-12 flex justify-between">
                 <h1 className="text-xl font-bold text-cyan-500 font-burtons">DevByKwame</h1>
                 <ul className="flex  items-center">
-                    <li> <BsFillMoonStarsFill onClick={()=>setDarkMode(!darkMode)} className="cursor-pointer" /> </li>
+                    <li> <BsFillMoonStarsFill onClick={()=>setDarkMode(!darkMode)} className="cursor-pointer dark:text-white" /> </li>
                     <li><a className="bg-cyan-500 text-white px-4 py-2 rounded ml-8" href="#">Resume</a></li>
                 </ul>
             </nav>
