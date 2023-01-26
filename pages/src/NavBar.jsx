e7467ca8eacc4fe23f5975ff77b4fea4309b048c
kwamea236 <kwamea236@protonmail.com>
2023-01-26 14:03:25 +0000

    still on cards

--- pages/src/NavBar.jsx
+++ pages/src/NavBar.jsx
@@ -2,11 +2,10 @@ import {BsFillMoonStarsFill} from "react-icons/bs";
 import { useState } from "react";
 
 function NavBar({darkMode, setDarkMode}){
-    const [navBarOpen, setNavBarOpen ] = useState(false);
     return(
         <div>
              <nav className="py-6 mb-1 flex justify-between">
-                <h1 className="text-xl font-bold text-cyan-500 font-burtons">DevByKwame</h1>
+                <h1 className="text-xl font-bold text-cyan-500 font-burtons">Whiz-Kwame</h1>
                 <ul className="flex  items-center">
                     <li> <BsFillMoonStarsFill onClick={()=>setDarkMode(!darkMode)} className="cursor-pointer dark:text-white" /> </li>
                     <li><a className="bg-cyan-500 text-white px-4 py-2 rounded ml-8" href="#">Resume</a></li>
