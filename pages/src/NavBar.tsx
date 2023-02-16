120636a0b19c66ed17bc973218220e80ba6830b9
kwamea236 <kwamea236@protonmail.com>
2023-02-16 09:46:57 +0000

    button cv

--- pages/src/NavBar.tsx
+++ pages/src/NavBar.tsx
@@ -13,7 +13,7 @@ function NavBar({darkMode, setDarkMode}: Props){
                 <ul className="flex  items-center">
                     <li className="px-5 rounded font-bold cursor-pointer hidden md:flex dark:text-white"><a>Project</a></li>
                     <li> <BsFillMoonStarsFill onClick={()=>setDarkMode(!darkMode)} className="cursor-pointer ml-1 dark:text-white" /> </li>
-                    <li><a className="bg-cyan-500 text-white ml-1/2 px-4 py-2 rounded ml-8" href="#">Download CV</a></li>
+                    <li><a className="bg-cyan-500 text-white ml-1/2 px-4 py-2 rounded ml-8 font-bold" href="#">CV</a></li>
                 </ul>
             </nav>
         </div>
