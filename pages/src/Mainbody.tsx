3dd754d4966285c5b8346f1a15d4b226f02b1d90
kwamea236 <kwamea236@protonmail.com>
2023-03-11 11:00:27 +0000

    hero page changed

--- pages/src/Mainbody.tsx
+++ pages/src/Mainbody.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 
 function Mainbody(){
     return(
-        <section>
+        <section className="">
             <div className="text-center p-10 mt-10">
-              <h3 className="text-5xl py-2 font-bold text-white font-poppins dark:text-white">UPGRADE YOUR BUSINESS</h3>
+              <h3 className="text-8xl py-2 px-20 font-bold text-white font-poppins text-center dark:text-white sm:text-5xl">Software is the soul of your business.</h3>
 
               <p className="text-xl py-5 leading-8 text-white font-medium font-poppins md:container md:mx-auto md:px-20 dark:text-white">
-                With Custom Built Website Designs and Web Applications, Optimised for SEO
-                and Built with Modularity Intent for Better Product development and experience.
+               We help organisation build software that unlocks their true potential,
+               propelling them through make-or-break moment with clarity and confidence.
               </p>
             </div>
 
             <div className="text-center">
               <Link href="/contact">
-                <button className="px-20 py-3 bg-white text-black font-bold cursor-pointer">Contact Us</button>
+                <button className="px-20 py-3 bg-white text-black font-bold cursor-pointer">See How We Can Help You</button>
               </Link>
               
             </div>
